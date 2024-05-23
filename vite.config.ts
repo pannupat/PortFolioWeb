31cdb133b227b81d68a910d63afac8c0fddaf267
Panupat Phonyiam <127047015+pannupat@users.noreply.github.com>
2024-05-23 13:44:38 +0700

    [Add]:Profile_Web

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import WindiCSS from 'vite-plugin-windicss'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),
+    WindiCSS(),
+    
+  ],
+  base:"/profile",
+
+
+  server:{
+    open:true,
+    port:9000,
+    proxy:{
+      '/api':{
+        target: 'http://localhost.9000',
+        changeOrigin:true,
+        rewrite:(path)=>path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
